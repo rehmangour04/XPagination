7c55fa6139d4408b8b0553c61c2616fa7dfb9fe4
Ataur Rehman <rehmangour04@gmail.com>
2024-03-29 15:35:34 +0530

    completed now with paginiation

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,82 @@
 /** @format */
 
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import "./App.css";
+import { useEffect, useState } from "react";
+import Pagination from "./components/Pagination";
 
-const App = () => {
-  const [data, setData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
+const API =
+  "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+
+const RESULTS_PER_PAGE = 10;
+
+function App() {
+  const [empData, setEmpData] = useState([]);
+  const [empPerPage, setEmpPerPage] = useState([]);
+  const [currPage, setCurrPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchEmployeeData = async () => {
+    try {
+      const res = await fetch(API);
+      const data = await res.json();
+      setEmpData(data);
+      setLoading(false);
+    } catch (err) {
+      setError("Failed to fetch data");
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    fetchData();
+    fetchEmployeeData();
   }, []);
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(
-        "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
-      );
-      setData(response.data);
-    } catch (error) {
-      alert("Failed to fetch data");
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  const totalPages = Math.ceil(data.length / 10);
-
-  const nextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const prevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-
-  const startIndex = (currentPage - 1) * 10;
-  const endIndex = Math.min(startIndex + 10, data.length);
+  useEffect(() => {
+    const start = (currPage - 1) * RESULTS_PER_PAGE;
+    const end = start + RESULTS_PER_PAGE;
+    setEmpPerPage(empData.slice(start, end));
+  }, [empData, currPage]);
 
   return (
-    <div>
-      <h1 className="title">Employee Data Table</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.slice(startIndex, endIndex).map((employee, index) => (
-            <tr key={index}>
-              <td>{employee.id}</td>
-              <td>{employee.name}</td>
-              <td>{employee.email}</td>
-              <td>{employee.role}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <div>
-        <button onClick={prevPage} disabled={currentPage === 1}>
-          Previous
-        </button>
-        <span className="page-info"> {currentPage} </span>
-        <button onClick={nextPage} disabled={currentPage === totalPages}>
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <header>
+        <h1>Employee Data Table</h1>
+      </header>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+              </tr>
+            </thead>
+            <tbody>
+              {empPerPage.map((emp) => (
+                <tr key={emp.id}>
+                  <td>{emp.id}</td>
+                  <td>{emp.name}</td>
+                  <td>{emp.email}</td>
+                  <td>{emp.role}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Pagination
+            page={currPage}
+            maxPage={Math.ceil(empData.length / RESULTS_PER_PAGE)}
+            setPage={setCurrPage}
+          />
+        </>
+      )}
+    </>
   );
-};
+}
 
 export default App;
